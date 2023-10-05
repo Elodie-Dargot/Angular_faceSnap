97016e7bf382f86aae3671530e141f911d671f2f
Elodie <elodie.dargot@gmail.com>
2023-10-05 09:18:52 +0200

    ajout guard

--- src/app/face-snaps/face-snaps.module.ts
+++ src/app/face-snaps/face-snaps.module.ts
@@ -5,7 +5,7 @@ import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
-import { FaceSnapRoutingModule } from './face-snap-routing.module';
+import { FaceSnapRoutingModule } from './face-snaps-routing.module';
 
 
 
